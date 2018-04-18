f3d35b4e0ed44b83faafb0c0e632a7fe1d73b2e5
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-04-18 12:53:08 +0200

    Fixade namn

--- eventkalender-client/src/main/java/cali/eventkalender/servlet/EventServlet.java
+++ eventkalender-client/src/main/java/cali/eventkalender/servlet/EventServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class EventServlet
  */
-@WebServlet("/EventServlet")
+@WebServlet("/events")
 public class EventServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
--- eventkalender-client/src/main/java/cali/eventkalender/servlet/NationServlet.java
+++ eventkalender-client/src/main/java/cali/eventkalender/servlet/NationServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class NationServlet
  */
-@WebServlet("/NationServlet")
+@WebServlet("/nations")
 public class NationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
