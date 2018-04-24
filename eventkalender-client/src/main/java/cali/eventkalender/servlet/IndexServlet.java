ad2e31cf128bb5412fbeb5a57a057dd53c32b56b
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-04-24 13:20:32 +0200

    Testar väder
    oh yes vi har nu väder

--- eventkalender-client/src/main/java/cali/eventkalender/servlet/IndexServlet.java
+++ eventkalender-client/src/main/java/cali/eventkalender/servlet/IndexServlet.java
@@ -10,13 +10,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import cali.eventkalender.facade.FacadeLocal;
 import cali.eventkalender.model.Event;
+import net.aksingh.owmjapis.api.APIException;
+import net.aksingh.owmjapis.core.OWM;
+import net.aksingh.owmjapis.model.CurrentWeather;
+import net.aksingh.owmjapis.model.param.Weather;
 
 @WebServlet("")
 public class IndexServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(IndexServlet.class);
+	
 	@EJB
 	private FacadeLocal facade;
 	
@@ -26,7 +36,23 @@ public class IndexServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+	    
+	    OWM owm = new OWM("e33f7134856ab9fbbe79a544cc23bc39");
+	    owm.setUnit(OWM.Unit.METRIC);
+	    try {
+            CurrentWeather cwd = owm.currentWeatherByCityName("Lund", OWM.Country.SWEDEN);
+            double temp = cwd.getMainData().getTemp();
+            Weather weather = cwd.getWeatherList().get(0);
+            LOGGER.info(weather.getIconLink());
+            LOGGER.info(String.valueOf(temp));
+            
+            request.setAttribute("weatherlink", weather.getIconLink());
+            
+        } catch (APIException e) {
+            // TODO Auto-generated catch block
+            LOGGER.info(e.getMessage());
+        }
+	    
 		List<Event> events = facade.findAllEvents();
 		request.setAttribute("events", events);
 		request.getRequestDispatcher("/pages/index.jsp").forward(request, response);
--- eventkalender-client/src/main/webapp/pages/index.jsp
+++ eventkalender-client/src/main/webapp/pages/index.jsp
@@ -37,5 +37,6 @@
 	<footer>
 		<%@ include file = "Footer.jsp" %>
 	</footer>
+	<img src="<%= request.getAttribute("weatherlink") %>">
 </body>
 </html>
