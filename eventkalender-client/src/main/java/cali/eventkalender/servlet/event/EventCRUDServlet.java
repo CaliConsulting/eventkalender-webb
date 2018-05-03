14808344ed557b4b4de453f524a8b387e1867e27
h4xed <nek15hsa@student.lu.se>
2018-05-03 13:37:00 +0200

    Integration för <select>-taggar i EventCRUD

--- eventkalender-client/src/main/java/cali/eventkalender/servlet/event/EventCRUDServlet.java
+++ eventkalender-client/src/main/java/cali/eventkalender/servlet/event/EventCRUDServlet.java
@@ -1,11 +1,18 @@
 package cali.eventkalender.servlet.event;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import cali.eventkalender.facade.FacadeLocal;
+import cali.eventkalender.model.Event;
+import cali.eventkalender.model.Nation;
 
 /**
  * Servlet implementation class EventCRUDServlet
@@ -13,7 +20,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/events/crud")
 public class EventCRUDServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    @EJB
+    private FacadeLocal facade;
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,6 +35,10 @@ public class EventCRUDServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		List<Nation> nations = facade.findAllNations();
+		request.setAttribute("nations", nations);
+		List<Event> events = facade.findAllEvents();
+		request.setAttribute("events", events);
 		request.getRequestDispatcher("/pages/EventCRUD.jsp").forward(request, response);
 	}
 
