5f32b4d7270fa13bb20815a6ee826a25f87c0a3f
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-05-04 19:04:36 +0200

    Fixade med JavaScript

--- eventkalender-client/src/main/webapp/js/custom-scripts.js
+++ eventkalender-client/src/main/webapp/js/custom-scripts.js
@@ -53,36 +53,3 @@ $(document).ready(function() {
 		})
 	}
 });
-
-//function buildCalendar(data) {
-//	var initialLocaleCode = 'sv';
-//
-//	console.log("startar kalender");
-//
-//	console.log("hej");
-//
-//	$('#calendar').fullCalendar({
-//		header : {
-//			left : 'prev,next today',
-//			center : 'title',
-//			//right : 'month,agendaWeek,agendaDay,listMonth'
-//			right : 'month'
-//		},
-//		locale : initialLocaleCode,
-//		editable : false,
-//		events : {
-//			url : 'calendar',
-//			type : 'POST',
-//			error : function() {
-//				alert('there was an error while fetching events!');
-//			}
-//		}
-//	})
-//}
-//
-//function getEvents() {
-//	$.post("calendar", function(data, status) {
-//		alert("Data: " + data);
-//		buildCalendar(data);
-//	});
-//}
