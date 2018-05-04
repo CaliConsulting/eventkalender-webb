1d4ad85506717bf74c79b14466b585479f58e3a3
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-05-04 20:01:11 +0200

    Lade till tid i konvertering

--- eventkalender-client/src/main/java/cali/eventkalender/utility/JsonUtility.java
+++ eventkalender-client/src/main/java/cali/eventkalender/utility/JsonUtility.java
@@ -1,5 +1,6 @@
 package cali.eventkalender.utility;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import com.google.gson.JsonArray;
@@ -10,12 +11,13 @@ import cali.eventkalender.model.Event;
 public class JsonUtility {
 
     public static String toCalendarJson(List<Event> events) {
+    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
         JsonArray array = new JsonArray();
         for (Event e : events) {
             JsonObject obj = new JsonObject();
             obj.addProperty("title", e.getName());
-            obj.addProperty("start", e.getStartTime().toLocalDate().toString());
-            obj.addProperty("end", e.getEndTime().toLocalDate().toString());
+            obj.addProperty("start", e.getStartTime().format(formatter));
+            obj.addProperty("end", e.getEndTime().format(formatter));
             array.add(obj);
         }
         return array.toString();
