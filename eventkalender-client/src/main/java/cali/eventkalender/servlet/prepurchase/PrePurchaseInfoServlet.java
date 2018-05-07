9bf961f84372137d705738dff1f053dacf494356
PhilipEriksson <35528685+PhilipEriksson@users.noreply.github.com>
2018-05-07 13:57:51 +0200

    Fix

--- eventkalender-client/src/main/java/cali/eventkalender/servlet/prepurchase/PrePurchaseInfoServlet.java
+++ eventkalender-client/src/main/java/cali/eventkalender/servlet/prepurchase/PrePurchaseInfoServlet.java
@@ -7,18 +7,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/prepurchase")
+@WebServlet("/prepurchase/info")
 public class PrePurchaseInfoServlet extends HttpServlet {
-	
-    private static final long serialVersionUID = 1L;
-       
+    
+	private static final long serialVersionUID = 1L;
+
     public PrePurchaseInfoServlet() {
         super();
     }
     
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/pages/PrePurchaseOverview.jsp").forward(request, response);
+		request.getRequestDispatcher("/pages/PrePurchaseInfo.jsp").forward(request, response);
 	}
    
 }
