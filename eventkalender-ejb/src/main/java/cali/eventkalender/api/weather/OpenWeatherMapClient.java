bfd79b5f69e60b77aa3addb2301e0bd595f13927
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-05-08 12:44:31 +0200

    Kollar om API:t lever

--- eventkalender-ejb/src/main/java/cali/eventkalender/api/weather/OpenWeatherMapClient.java
+++ eventkalender-ejb/src/main/java/cali/eventkalender/api/weather/OpenWeatherMapClient.java
@@ -1,5 +1,12 @@
 package cali.eventkalender.api.weather;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.aksingh.owmjapis.api.APIException;
 import net.aksingh.owmjapis.core.OWM;
 import net.aksingh.owmjapis.model.CurrentWeather;
@@ -10,6 +17,8 @@ import net.aksingh.owmjapis.model.param.Weather;
  */
 public class OpenWeatherMapClient {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(OpenWeatherMapClient.class);
+    
     private static OpenWeatherMapClient client;
 
     private OWM owm;
@@ -32,27 +41,52 @@ public class OpenWeatherMapClient {
 
     public CurrentWeather getCurrentWeather(String city) {
         try {
-            CurrentWeather cwd = owm.currentWeatherByCityName("Lund", OWM.Country.SWEDEN);
-            return cwd;
+            if (isConnectionValid()) {
+                CurrentWeather cwd = owm.currentWeatherByCityName("Lund", OWM.Country.SWEDEN);
+                return cwd;
+            }
+            return null;
         } catch (APIException e) {
             throw new RuntimeException(e);
         }
     }
 
     public String getTemperature(String city) {
-        double temp = getCurrentWeather(city).getMainData().getTemp();
+        CurrentWeather cwd = getCurrentWeather(city);
+        if (cwd == null) {
+            return "--";
+        }
+        double temp = cwd.getMainData().getTemp();
         // Round temperature
         return String.format("%.1f", temp);
     }
 
     public String getIconLink(String city) {
         CurrentWeather cwd = getCurrentWeather(city);
+        if (cwd == null) {
+            // Fullösning men låt gå, om nu API:t skulle gå ner
+            return "/EventkalenderClient/img/questionmark.png";
+        }
         if (cwd.hasWeatherList()) {
             // We want the current weather; it is first in the list
             Weather weather = cwd.getWeatherList().get(0);
             return weather.getIconLink();
         }
         return null;
+    }
+    
+    private boolean isConnectionValid() {
+        String strUrl = "https://api.openweathermap.org/data/2.5/";
+        try {
+            URL url = new URL(strUrl);
+            HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+            urlConn.connect();
+            LOGGER.info("valid connection");
+            return true;
+        } catch (IOException e) {
+            LOGGER.info("invalid connection");
+            return false;
+        }
     }
 
 }
